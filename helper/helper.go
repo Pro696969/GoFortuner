d8224594e583a5043a052dc9fb708e8e39ffd882
Pro696969 <krishnakumaarmc@gmail.com>
2024-07-30 11:55:57 +0530

    printing random file names

--- helper/helper.go
+++ helper/helper.go
@@ -3,12 +3,18 @@ package helper
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 )
+
 var files []string
+
+func RandInt(min int, max int) int {
+  return min + rand.Intn(max-min)
+}
 
 func PathWalk(path string, f os.FileInfo, err error) error {
   if filepath.Ext(path) == ".dat" {
@@ -40,15 +46,22 @@ func Program() {
   // fmt.Println(path)
   err = filepath.Walk(root, PathWalk)
 
-  // printing the file tree structure 
-  // for i := 1; i < len(files); i++ {
-  //   fmt.Println(files[i])
-  // }
-
-  // fmt.Println(files)
   if err != nil {
     panic(err)
   }
 
 
+
+  // printing the file tree structure 
+  // for i := 1; i < len(files); i++ {
+  //   fmt.Println(files[i])
+  // }
+  // fmt.Println(len(files)) 6 files found
+  // fmt.Println(files)
+
+  random_num := RandInt(1,len(files))
+  random_filename := files[random_num]
+  fmt.Println(random_filename)
+
 }
+
