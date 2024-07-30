44524ab3099f10ff67a5fda75cc4ab2fafaa3743
Pro696969 <krishnakumaarmc@gmail.com>
2024-07-30 11:08:50 +0530

    file walk fortune

--- helper/helper.go
+++ helper/helper.go
@@ -1,10 +1,27 @@
 package helper
 
 import (
-  "bufio"
-  "fmt"
-  "os/exec"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
+var files []string
+
+func PathWalk(path string, f os.FileInfo, err error) error {
+  if filepath.Ext(path) == ".dat" {
+    return nil
+  }
+
+  if f.IsDir() {
+    return nil
+  }
+
+  files = append(files, path)
+  return nil
+}
 
 func Program() {
   fortuneCommand := exec.Command("fortune", "-f")
@@ -17,5 +34,21 @@ func Program() {
   fortuneCommand.Start()
   outputStream := bufio.NewScanner(pipe)
   outputStream.Scan()
-  fmt.Println(outputStream.Text())
+  line := outputStream.Text()
+  root := line[strings.Index(line,"/"):]  //  extract - /usr/share/games/fortunes all fortunes are found here
+  
+  // fmt.Println(path)
+  err = filepath.Walk(root, PathWalk)
+
+  // printing the file tree structure 
+  // for i := 1; i < len(files); i++ {
+  //   fmt.Println(files[i])
+  // }
+
+  // fmt.Println(files)
+  if err != nil {
+    panic(err)
+  }
+
+
 }
