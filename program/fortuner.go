9c8cfde88cbc8647cabf8f19b02840b0446aefa5
Pro696969 <krishnakumaarmc@gmail.com>
2024-07-31 12:12:29 +0530

    refactor

--- program/fortuner.go
+++ program/fortuner.go
@@ -15,34 +15,40 @@ func err_handler(e error) {
 }
 
 func random_intn(min, max int) int {
-  return min + rand.Intn(max - min)
+	return min + rand.Intn(max-min)
+}
+
+func random_fortuner_all() *os.File {
+	f, err := os.Open("assets/all_fortunes")
+	err_handler(err)
+	return f
 }
 
 func Program() {
-	f, err := os.Open("assets/all_fortunes")
-	err_handler(err)
-  defer f.Close()
+	// cli_args := os.Args
+
 	// fmt.Println(string(f))
-  // scanner := bufio.NewScanner(f)
-  // scanner.Split(bufio.ScanLines)
-  //
-  // count := 0
-  // for scanner.Scan() {
-  //   count++
-  // }
-  //
-  // fmt.Println("number of lines scanned ", count)
+	// scanner := bufio.NewScanner(f)
+	// scanner.Split(bufio.ScanLines)
+	//
+	// count := 0
+	// for scanner.Scan() {
+	//   count++
+	// }
+	//
+	// fmt.Println("number of lines scanned ", count)
 
-  b, err := io.ReadAll(f)
-  err_handler(err)
-  b_string := string(b)  
-  
-  // fmt.Println(b_string)
+	f := random_fortuner_all()
+	defer f.Close()
+	b, err := io.ReadAll(f)
+	err_handler(err)
+	b_string := string(b)
 
-  fortuner_array := strings.Split(b_string, "%")
-  random_fortune := random_intn(1,len(fortuner_array))
+	// fmt.Println(b_string)
 
-  fmt.Println(fortuner_array[random_fortune])
+	fortuner_array := strings.Split(b_string, "%")
+	random_fortune := random_intn(1, len(fortuner_array))
 
+	fmt.Println(fortuner_array[random_fortune])
 
 }
